c7c1b2caf042843f04b2cd21d9000d481ee25995
Rasmussen <Karl.Rasmussen@henryschein.com>
2018-12-28 16:57:59 -0700

    Added update

---
+++ playground/mongodb-update.js
@@ -0,0 +1,42 @@
+// khr test
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true}, (err, client) => {
+    if (err) {
+        return console.log('Unable to connct to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5c26a1496410b2bf2f0b98e0')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5c266f44eab5c45efcdaec1a')
+    }, {
+        $set: {
+            name: 'Kami Rasmussen'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    
+
+    //client.close();
+});
