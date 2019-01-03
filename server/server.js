463c60c1654274254f9859fcd0f9ccb49cfa7394
Rasmussen <Karl.Rasmussen@henryschein.com>
2019-01-03 13:29:10 -0700

    fixed User path

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var  {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {User} = require('./models/User');
+var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
