7d460f386c0c14d7cf42ca3326a6e670212eb77a
Rasmussen <Karl.Rasmussen@henryschein.com>
2019-01-03 14:43:58 -0700

    test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promose = global.Promise;
-//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',  {useNewUrlParser: true});
-mongoose.connect('mongodb://karlrasmussen:mkarl0907@ds229552.mlab.com:29552/nodejsclass', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',  {useNewUrlParser: true});
+//mongoose.connect('mongodb://karlrasmussen:mkarl0907@ds229552.mlab.com:29552/nodejsclass', {useNewUrlParser: true});
 module.exports = {mongoose};
-//test
