b5f68c276b5715b2f7f1712469a987191d69e6e6
Rasmussen <Karl.Rasmussen@henryschein.com>
2019-01-04 16:48:45 -0700

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,11 @@ var mongoose = require('mongoose');
 
 mongoose.Promose = global.Promise;
 
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://karlrasmussen:mkarl0907@ds229552.mlab.com:29552/nodejsclass'
-  };
-mongoose.connect( process.env.PORT ? db.mlab : db.localhost,  {useNewUrlParser: true}); 
+// let db = {
+//     localhost: 'mongodb://localhost:27017/TodoApp',
+//     mlab: 'mongodb://karlrasmussen:mkarl0907@ds229552.mlab.com:29552/nodejsclass'
+//   };
+// mongoose.connect( process.env.PORT ? db.mlab : db.localhost,  {useNewUrlParser: true}); 
+mongoose.connect(process.env.MONGODB_URI,  {useNewUrlParser: true}); 
 
 module.exports = {mongoose};
